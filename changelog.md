32fe9e9f375978d258fecf3883599d76619218cf
Alphalaneous <38200084+Alphalaneous@users.noreply.github.com>
2025-05-28 01:02:14 -0400

    Improvements

--- changelog.md
+++ changelog.md
@@ -1,2 +1,6 @@
+# 1.0.1
+- Performance Improvements
+- Update slider on object placement
+
 # 1.0.0
 - Initial Release
--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,35 @@ using namespace geode::prelude;
 
 class $modify(MyEditorUI, EditorUI) {
 	
+	struct Fields {
+		int m_lastObjectCount = 0;
+		float m_lastObjectX = 0.f;
+	};
+
+	void checkForObjectChange(float dt) {
+		auto fields = m_fields.self();
+
+		int last = fields->m_lastObjectCount;
+		int cur = m_editorLayer->m_objectCount;
+
+		if (last != cur) {
+			fields->m_lastObjectX = m_editorLayer->getLastObjectX();
+			updateSlider();
+		}
+	
+		fields->m_lastObjectCount = m_editorLayer->m_objectCount;
+    }
+
+    bool init(LevelEditorLayer* editorLayer) {
+		if (!EditorUI::init(editorLayer)) return false;
+        schedule(schedule_selector(MyEditorUI::checkForObjectChange));
+		return true;
+	}
+
     void sliderChanged(cocos2d::CCObject*) {
-		float maxX = (m_editorLayer->getLastObjectX() + 300) * m_editorLayer->m_objectLayer->getScale();
+		auto fields = m_fields.self();
+
+		float maxX = (fields->m_lastObjectX + 300) * m_editorLayer->m_objectLayer->getScale();
 		float x = -(maxX * m_positionSlider->getThumb()->getValue()) + 100;
 		float y = m_editorLayer->m_objectLayer->getPositionY();
 		m_editorLayer->m_objectLayer->setPosition({x, y});
@@ -14,7 +41,9 @@ class $modify(MyEditorUI, EditorUI) {
 	}
 
 	void updateSlider() {
-		float maxX = (m_editorLayer->getLastObjectX() + 300) * m_editorLayer->m_objectLayer->getScale();
+		auto fields = m_fields.self();
+
+		float maxX = (fields->m_lastObjectX + 300) * m_editorLayer->m_objectLayer->getScale();
 		float x = -m_editorLayer->m_objectLayer->getPositionX() + 100;
 		float pos = std::clamp(x / maxX, 0.0f, 1.0f);
 		m_positionSlider->setValue(pos);
